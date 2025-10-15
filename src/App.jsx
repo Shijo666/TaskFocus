179c5d267f4480e99e45d9f45e060c06adc1804e
Shijo666 <sshijo666@gmail.com>
2025-10-15 11:25:10 +0530

    Third Commit-Changes on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Taskform from "./Components/Taskform";
-import TaskList from "./Components/Tasklist.jsx";
+import Tasklist from "./Components/Tasklist.jsx";
 import Progresstracker from "./Components/Progresstracker";
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
       <h1>Task Focus</h1>
       <p>Our friendly TaskManager</p>
       <Taskform />
-      <TaskList />
+      <Tasklist />
       <Progresstracker />
       <button>Clear all Tasks</button>
     </div>
